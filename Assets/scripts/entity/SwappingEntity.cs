56f117554070e89052fdab3b03da9fc5d7ae36cb
SirGFM <gfmandaji@hotmail.com>
2021-12-01 21:39:45 -0300

    entity: Add SwappingEntity
    Add a component for creating complex entity by alternating between simpler
    ones.

---
+++ Assets/scripts/entity/SwappingEntity.cs
@@ -0,0 +1,96 @@
+using GO = UnityEngine.GameObject;
+using Time = UnityEngine.Time;
+
+/**
+ * Component to sequentially alternate between multiple gameObject in a
+ * timely manner. Each object should implement one specific, for example
+ * a force field pushing upward and another pushing downward. Empty
+ * gameObjects are also allowed and simply cause that the activation of
+ * the next gameObject to be delayed.
+ *
+ * The DefaultTime is used if a custom time isn't specified for the object.
+ */
+public class SwappingEntity : UnityEngine.MonoBehaviour {
+
+	/** An entity that is enabled for some time. */
+	[System.Serializable]
+	public class TimedEntity {
+		/** The gameObject. */
+		public GO Object;
+		/** For how long the gameObject should stay active. */
+		public float Time;
+
+		/** Activate this object and return its active time, or 0 if not set. */
+		public float Activate() {
+			if (this.Object != null) {
+				this.Object.SetActive(true);
+			}
+
+			if (this.Time > 0.0f) {
+				return this.Time;
+			}
+			return 0.0f;
+		}
+
+		/** Deactivate this object. */
+		public void Deactivate() {
+			if (this.Object != null) {
+				this.Object.SetActive(false);
+			}
+		}
+	};
+
+	/** Objects that are sequentially enabled by this object. */
+	public TimedEntity[] Objects;
+
+	/** For how long objects should be enabled, when not specified. */
+	public float DefaultTime = 5.0f;
+
+	/** Index of the current object. */
+	private int idx;
+
+	/** Time until the object must be disabled. */
+	private float dt;
+
+	void Start() {
+		this.idx = 0;
+
+		/* Disable every object but the first one. */
+		for (int i = 0; i < this.Objects.Length; i++) {
+			GO obj = this.Objects[i].Object;
+			if (obj != null) {
+				obj.SetActive(false);
+			}
+		}
+
+		if (this.Objects.Length > 0) {
+			this.dt = this.Objects[0].Activate();
+			if (this.dt == 0.0f) {
+				this.dt = this.DefaultTime;
+			}
+		}
+	}
+
+	void Update() {
+		this.dt -= Time.deltaTime;
+		if (this.dt <= 0) {
+			if (this.Objects.Length == 0) {
+				return;
+			}
+
+			this.Objects[this.idx].Deactivate();
+
+			this.idx++;
+			if (this.idx >= this.Objects.Length) {
+				this.idx = 0;
+			}
+
+			float newDt = this.Objects[this.idx].Activate();
+			if (newDt == 0.0f) {
+				newDt = this.DefaultTime;
+			}
+
+			this.dt += newDt;
+		}
+	}
+}
