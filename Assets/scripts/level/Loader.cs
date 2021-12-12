aace93b22a9b3e78e09e7f989d20c62bb0b0e01f
SirGFM <gfmandaji@hotmail.com>
2021-12-12 13:59:14 -0300

    scenes: Add Loader
    Create the Loader scene, which loads levels in the background, managing
    the UI as necessary.

--- Assets/scripts/level/Loader.cs
+++ Assets/scripts/level/Loader.cs
@@ -1,5 +1,27 @@
+using AsyncOp = UnityEngine.AsyncOperation;
 using EvSys = UnityEngine.EventSystems;
+using Scene = UnityEngine.SceneManagement.Scene;
+using SceneMng = UnityEngine.SceneManagement.SceneManager;
+using SceneMode = UnityEngine.SceneManagement.LoadSceneMode;
 using UiText = UnityEngine.UI.Text;
+
+/**
+ * Loader loads levels in the background, managing the UI as necessary.
+ * This component also assigns itself as the game's Root object, so it
+ * may handle events from any loaded scene.
+ *
+ * While a stage is being loaded, the UI must send this object a
+ * SetProgressBar event to signal which object has the UI, so the loader
+ * may update it as the level is loaded. Similarly, the UI must send a
+ * SetSceneTitle to set the name on the loading screen and within the
+ * in-game UI.
+ *
+ * This component also implementns OnReset, to reload the current stage
+ * and OnGoal to load the next stage. The stage that will be loaded by
+ * this component is stored in the global variable currentLevel, which
+ * must be manually set to start a game from the first level, or to start
+ * from the selected level.
+ */
 
 /** Identify UI scenes that may try to ask for the scene's name. */
 public enum UiScene {
@@ -33,5 +55,130 @@ public interface LoaderIface : EvSys.IEventSystemHandler {
 	void OnReset();
 }
 
-public class Loader : UnityEngine.MonoBehaviour {
+public class Loader : BaseRemoteAction, LoaderIface, GoalIface {
+	/** XXX: The first scene in the game **must** be the mainmenu, while the
+	 * second one is the first stage...
+	 * To reset the game back to the first stage, this must be manually
+	 * set back to 1. */
+	static public int currentLevel = 1;
+
+	/** Name of the sub-scene used to display the loading progress. */
+	public string uiScene = "LoadingUI";
+
+	/** Name of the sub-scene used to display the game UI. */
+	public string gameUiScene = "GameUI";
+
+	/** UI progress bar. */
+	private ProgressBar pb;
+
+	/** The loader scene, used to reload it. */
+	private int loaderScene;
+
+	/** Whether a new scene is being loaded. */
+	private bool loadingNew;
+
+	/** Wehther resetting is blocked, as while the stage is being loaded. */
+	private bool blockReset;
+
+	void Start() {
+		this.loadingNew = false;
+		this.blockReset = true;
+
+		this.loaderScene = SceneMng.GetActiveScene().buildIndex;
+
+		BaseRemoteAction.setRootTarget(this.gameObject);
+		this.StartCoroutine(this.load());
+	}
+
+	/**
+	 * Reload the current scene. The actual stage that should be loaded
+	 * depends on Loader.currentLevel's value.
+	 */
+	private void reloadScene() {
+        SceneMng.LoadSceneAsync(this.loaderScene, SceneMode.Single);
+		this.loadingNew = true;
+	}
+
+	public void OnGoal() {
+		if (this.loadingNew) {
+			return;
+		}
+
+		Loader.currentLevel++;
+
+		/* Eventually, a game over scene which loops back to the main menu
+		 * should be loaded. Therefore, currentLevel shouldn't ever
+		 * overflow in a release build. However, this is useful when
+		 * running in the editor to avoid issues. */
+#if UNITY_EDITOR
+		if (Loader.currentLevel >= SceneMng.sceneCountInBuildSettings) {
+			UnityEngine.Debug.LogWarning("Tried to load a scene past the last index! Going back to the first level...");
+			Loader.currentLevel = 1;
+		}
+#endif
+
+		this.reloadScene();
+	}
+
+	public void OnReset() {
+		if (this.blockReset || this.loadingNew) {
+			return;
+		}
+
+		this.reloadScene();
+	}
+
+	public void SetProgressBar(out bool done, ProgressBar pb) {
+		this.pb = pb;
+		done = true;
+	}
+
+	public void SetSceneTitle(out bool done, UiText txt, UiScene ui) {
+		string separator;
+		string levelName = LevelNameList.GetLevel(currentLevel);
+
+		switch (ui) {
+		case UiScene.LoadingScene:
+			separator = "\n";
+			break;
+		case UiScene.GameScene:
+			separator = ": ";
+			break;
+		default:
+			throw new System.Exception($"Invalid UiScene ({ui})");
+		}
+
+		txt.text = $"Level {Loader.currentLevel}{separator}{levelName}";
+		done = true;
+	}
+
+	/**
+	 * Loads the current stage in background, updating the progress bar
+	 * (if it was configured) as the stage is loaded.
+	 */
+	private System.Collections.IEnumerator load() {
+		AsyncOp op;
+
+		this.pb = null;
+		op = SceneMng.LoadSceneAsync(this.uiScene, SceneMode.Additive);
+		yield return op;
+
+		op = SceneMng.LoadSceneAsync(currentLevel, SceneMode.Additive);
+		while (op.progress < 1.0f) {
+			/* Update a progress bar */
+			if (this.pb != null)
+				this.pb.progress = op.progress * 0.95f;
+			yield return new UnityEngine.WaitForFixedUpdate();
+		}
+
+		SceneMng.UnloadSceneAsync(this.uiScene);
+
+		/* TODO: Load the game UI */
+#if false
+		op = SceneMng.LoadSceneAsync(this.gameUiScene, SceneMode.Additive);
+		yield return op;
+#endif
+
+		this.blockReset = false;
+	}
 }
---
+++ Assets/scripts/util/LevelNameList.cs
@@ -0,0 +1,127 @@
+using CultureInfo = System.Globalization.CultureInfo;
+using Scene = UnityEngine.SceneManagement.Scene;
+using SceneMng = UnityEngine.SceneManagement.SceneManager;
+using SceneUtil = UnityEngine.SceneManagement.SceneUtility;
+using TextInfo = System.Globalization.TextInfo;
+
+/**
+ * LevelNameList caches every scene name on the build. It can either be
+ * accessed directly, by calling GetLevel(), or by retrieving the array of
+ * name, by calling Get().
+ *
+ * Since Unity uses the full path to the scene file as its name, this class
+ * removes the directories from the level name, as well as the .unity
+ * extension. Also, the class assumes that a level name always start with
+ * a digit.
+ *
+ * If the name has any spaces, no extra conversion is done. Otherwise,
+ * every word is capitalized, dashes ('-') are converted into spaces and
+ * double dashes are converted into single dashes.
+ *
+ * So, a level like 'Assets/scenes/00-basic/00-first-steps.unity' is
+ * converted into something like 'First Steps'. Meanwhile, a level named
+ * something like 'Assets/scenes/00-basic/00-first steps.unity' wouldl be
+ * converted into 'first steps'.
+ */
+
+static public class LevelNameList {
+	/** Cache the list of level names. */
+	static private string[] _list = null;
+
+	/**
+	 * Convert the input name, a scene filename, into a level name.
+	 *
+	 * Everything until the first character is removed. Then, single
+	 * dashes ('-') are converted into spaces and double dashes are
+	 * converted into singles dashes. Lastly, every word is capitalized.
+	 *
+	 * However, if the filename already has spaces, no extra conversion is
+	 * done and the filename is used as is after removing the initial
+	 * number and the extension.
+	 *
+	 * @param baseName: The scene name, as retrieved from Unity.
+	 * @return The processed name.
+	 */
+	static private string ProcessName(string baseName) {
+		int start = baseName.LastIndexOf("/");
+		int end = baseName.LastIndexOf(".");
+
+		/* Extract the filename. */
+		baseName = baseName.Substring(start + 1, end - start - 1);
+
+		/* Remove dashes, converting it into spaces. */
+		bool convert = (baseName.IndexOf(" ") == -1);
+		if (convert) {
+			baseName = baseName.Replace("--", "/");
+			baseName = baseName.Replace("-", " ");
+			baseName = baseName.Replace("/", "-");
+		}
+
+		/* Remove the initial number. */
+		for (int i = 0; i < baseName.Length; i++) {
+			if ((baseName[i] >= 'A' && baseName[i] <= 'Z') ||
+					(baseName[i] >= 'a' && baseName[i] <= 'z')) {
+				baseName = baseName.Substring(i);
+				break;
+			}
+		}
+
+		/* Capitalize every word. */
+		if (convert) {
+			TextInfo ti = new CultureInfo("en-US",false).TextInfo;
+			baseName = ti.ToTitleCase(baseName);
+		}
+
+		UnityEngine.Debug.Log($"{baseName}");
+		return baseName;
+	}
+
+	/**
+	 * Iterate over every filename, generating the internal list of level
+	 * names.
+	 */
+	static private void UpdateList() {
+		int i;
+		int max = SceneMng.sceneCountInBuildSettings;
+
+		for (i = 1; i < max; i++) {
+			string name = SceneUtil.GetScenePathByBuildIndex(i);
+			int pos = name.LastIndexOf("/");
+			char first = name[pos + 1];
+
+			/* Every level start with a number, so use this to find the
+			 * number of levels */
+			if (first < '0' && first > '9')
+				break;
+		}
+
+		max = i;
+		LevelNameList._list = new string[max];
+		for (i = 1; i < max; i++)
+			LevelNameList._list[i] = ProcessName(SceneUtil.GetScenePathByBuildIndex(i));
+	}
+
+	/**
+	 * Retrieve the list of level names.
+	 *
+	 * @return The list of level names.
+	 */
+	static public string[] Get() {
+		if (LevelNameList._list == null)
+			UpdateList();
+		return LevelNameList._list;
+	}
+
+	/**
+	 * Retrieve the name of a given level (starting at 1).
+	 *
+	 * @return The i-th level.
+	 */
+	static public string GetLevel(int i) {
+		if (LevelNameList._list == null)
+			UpdateList();
+		if (i < LevelNameList._list.Length)
+			return LevelNameList._list[i];
+		return "Unknown";
+	}
+}
