2b9b08112738b24172f153aa3249665f16a56f6f
SirGFM <gfmandaji@hotmail.com>
2022-08-13 17:05:34 -0300

    ui/menu/MainMenu: Adjust indentation

--- Assets/scripts/ui/menu/MainMenu.cs
+++ Assets/scripts/ui/menu/MainMenu.cs
@@ -3,28 +3,28 @@ using Color = UnityEngine.Color;
 using Image = UnityEngine.UI.Image;
 
 public class MainMenu : VerticalTextMenu {
-    private string[] _opts = {
-        "New game",
-        "Options",
-        "Quit"
-    };
+	private string[] _opts = {
+		"New game",
+		"Options",
+		"Quit"
+	};
 
-    override protected void onSelect() {
-        switch (this.getCurrentOpt()) {
-        case 0:
-            this.LoadLevel(1);
-            break;
-        case 1:
+	override protected void onSelect() {
+		switch (this.getCurrentOpt()) {
+		case 0:
+			this.LoadLevel(1);
+			break;
+		case 1:
 			this.LoadScene("scenes/menu/Options");
-            break;
-        case 2:
-            App.Quit();
-            break;
-        }
-    }
+			break;
+		case 2:
+			App.Quit();
+			break;
+		}
+	}
 
-    override protected void start() {
-        this.options = this._opts;
-        base.start();
-    }
+	override protected void start() {
+		this.options = this._opts;
+		base.start();
+	}
 }
